d0f9a0f7ee0f9ae87c5eaa3c65a5519f95e48095
Camilovelag <camilo.vela.galindo@gmail.com>
2022-10-11 15:13:44 -0500

    Add new score using js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import _ from 'lodash';
 import './style.css';
+import Score from './modules/score.js';
+import refreshpage from './modules/refresh.js';
+
+const form = document.querySelector('form');
+const refresh = document.getElementById('refresh');
+
+//refresh scoreboard
+
+refresh.addEventListener('click', (e) => {
+  e.preventDefault();
+  refreshpage();
+});
+
+//add new score
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+
+  const newScore = new Score(name, score);
+  newScore.renderScore();
+
+  form.reset();
+});
---
+++ src/modules/score.js
@@ -0,0 +1,15 @@
+const scores = document.getElementById('scores');
+
+export default class Score {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  //render score
+  renderScore() {
+    const li = `<li>${this.name} : ${this.score} (new)</li>`;
+    scores.insertAdjacentHTML('beforeend', li);
+  }
+
+}
