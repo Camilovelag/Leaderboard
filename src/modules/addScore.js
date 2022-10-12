de26f7490c0bf63cfc83af70585eec547e604d66
Camilovelag <camilo.vela.galindo@gmail.com>
2022-10-12 16:00:39 -0500

    Make async API requests

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,14 +1,12 @@
-const addScore = (score) => {
-  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5BiPQuuuQC4a8rdncjYV/scores/';
-  fetch(`${url}`, {
+const addScore = async (score) => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/iwN8wBNmYfSm0NhEOvs2/scores/';
+  await fetch(`${url}`, {
     method: 'POST',
     body: JSON.stringify(score),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  })
-    .then((response) => response.json())
-    .then((json) => json);
+  });
 };
 
 export default addScore;
--- src/modules/renderScores.js
+++ src/modules/renderScores.js
@@ -1,13 +1,11 @@
-const renderScores = () => {
-  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5BiPQuuuQC4a8rdncjYV/scores/';
+const renderScores = async () => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/iwN8wBNmYfSm0NhEOvs2/scores/';
   const scores = document.getElementById('scores');
 
-  fetch(`${url}`)
-    .then((response) => response.json())
-    .then((users) => {
-      const list = users.result.map((user) => `<li>${user.user} : ${user.score}</li></br>`).join('');
-      scores.innerHTML = `${list}`;
-    });
+  const response = await fetch(`${url}`);
+  const users = await response.json();
+  const list = users.result.map((user) => `<li>${user.user} : ${user.score}</li></br>`).join('');
+  scores.innerHTML = `${list}`;
 };
 
 export default renderScores;
