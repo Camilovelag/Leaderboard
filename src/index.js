0ed33980ee99d9d218f6f4b076293686902e7ee3
Camilovelag <camilo.vela.galindo@gmail.com>
2022-10-12 19:03:06 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ import renderScores from './modules/renderScores.js';
 const form = document.querySelector('form');
 const refresh = document.getElementById('refresh');
 const footer = document.querySelector('footer');
+const createGame = document.getElementById('newGame');
+
+// set game ID
+const gameId = 'BsB5KoZfAhoYwOzGkskM';
+localStorage.setItem('game-ID', gameId);
 
 // refresh scoreboard
-
 refresh.addEventListener('click', (e) => {
   e.preventDefault();
   renderScores();
@@ -18,10 +22,8 @@ refresh.addEventListener('click', (e) => {
 });
 
 // add new score
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
   const name = document.getElementById('name').value;
   const score = document.getElementById('score').value;
   const newScore = new Score(name, score);
@@ -30,7 +32,7 @@ form.addEventListener('submit', (e) => {
 });
 
 // create new game
-const createGame = document.getElementById('newGame');
+footer.style.display = 'none';
 createGame.addEventListener('click', (e) => {
   e.preventDefault();
   // eslint-disable-next-line no-alert, no-restricted-globals
@@ -39,7 +41,4 @@ createGame.addEventListener('click', (e) => {
   }
 });
 
-const gameId = 'BsB5KoZfAhoYwOzGkskM';
-localStorage.setItem('game-ID', gameId);
 renderScores();
-footer.style.display = 'none';
